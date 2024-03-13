5dc0a1c72201490bab51ddfcf273229440894a60
Anfas <anfasmuhammed936@gmail.com>
2024-03-13 12:37:42 +0530

    create database and env

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,14 @@
 const express=require('express')
 const app=express();
+const mongoose=require('mongoose');
+const dotenv=require('dotenv').config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Database connected");
+}).catch((err)=>{
+    console.log(err);
+})
 
 app.listen(3000,()=>{
     console.log("server running!!");
-})
+});
